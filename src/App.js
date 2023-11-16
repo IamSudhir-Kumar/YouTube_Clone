031b2e4873e89bf7ac5b6daaf099ffb6dc897d8b
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-11-16 14:33:10 +0530

    sidebar set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import { Box } from '@mui/material'
-import Navbar from './components/Navbar'
-import Feed from './components/Feed'
-import VideoDetail from './components/VideoDetail'
-import ChannelDetail from './components/ChannelDetail'
-import SearchFeed from './components/SearchFeed'
+
+import {Feed, SearchFeed, VideoDetail, ChannelDetail, Navbar} from './components/index'
+
 
 const app = () => {
   return (
