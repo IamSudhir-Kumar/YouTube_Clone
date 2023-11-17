fc829538f1263b6f564ba4535fefa9fce3498a8d
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-11-17 21:40:04 +0530

    vedio card
    Please enter the commit message for your changes. Lines starting

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import {Sidebar , Video} from './'
+import { Sidebar, Video } from './'
 import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const Feed = () => {
- const [selectedCategory, setSelectedCategory] = useState('New');
- const [videos, setVideos] = useState([]);
-  useEffect(() =>{
-    fetchFromAPI(`search?part=snnipet&q=${selectedCategory}`)
-    .then((data) => (setVideos(data.items)),
-    [selectedCategory])
-  } , [selectedCategory]);
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [video, setVideo] = useState([]);
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=
+  ${selectedCategory}`)
+      .then((data) => setVideo(data.items));
+  }, [selectedCategory]);
+
   return (
     <Stack sx={
       { flexDirection: { sx: 'column', md: 'row' } }
     }>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-        <Sidebar 
-        selectedCategory={selectedCategory}
-        setSelectedCategory={setSelectedCategory}
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
         />
 
         <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
@@ -43,7 +44,7 @@ const Feed = () => {
             Videos
           </span>
         </Typography>
-        <Video videos={videos}/>
+        <Video video={video} />
       </Box>
     </Stack>
   )
---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+
+import { demoThumbnail, demoVideoUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl } from '../utils/constant'
+
+const VideoCard = ({ video}) => {
+    console.log(video)
+    return (
+    <></>
+    
+  )
+}
+
+export default VideoCard
