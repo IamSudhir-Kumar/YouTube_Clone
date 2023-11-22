55eaf5d0499df0ca76a9481282a768c8978a36f9
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-11-22 11:52:50 +0530

    API problems

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
-import { Box, CardContent, CardMedia, Typography } from '@mui/material'
-import { Link } from 'react-router-dom'
-import { CheckCircle } from '@mui/icons-material'
+import React from 'react';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { Link } from 'react-router-dom';
+import { demoProfilePicture } from '../utils/constant';
 
-import { demoProfilePicture } from '../utils/constant'
-const ChannelCard = ({ channelDetail }) => (
-  <Box sx={{
-    boxShadow: 'none',
-    borderRadius: '20px',
-  }}>
+const ChannelCard = ({ channelDetail, marginTop }) => (
+  <Box
+    sx={{
+      boxShadow: 'none',
+      borderRadius: '20px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: { xs: '356px', md: '320px' },
+      height: '326px',
+      margin: 'auto',
+      marginTop,
+    }}
+  >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
       <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
         <CardMedia
-          image={channelDetail?.snippet?.thumbnails?.default?.url || demoProfilePicture}
+          image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
           alt={channelDetail?.snippet?.title}
-          sx={{ width: '180px', height: '180px', borderRadius: '50%' }}
+          sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
         />
-
+        <Typography variant="h6">
+          {channelDetail?.snippet?.title}{' '}
+          <CheckCircleIcon sx={{ fontSize: '14px', color: 'gray', ml: '5px' }} />
+        </Typography>
+        {channelDetail?.statistics?.subscriberCount && (
+          <Typography sx={{ fontSize: '15px', fontWeight: 500, color: 'gray' }}>
+            {parseInt(channelDetail?.statistics?.
+              subscriberCount).
+              toLocaleString('en-US')} Subscribers
+          </Typography>
+        )}
       </CardContent>
     </Link>
   </Box>
-)
+);
 
-export default ChannelCard
+export default ChannelCard;
